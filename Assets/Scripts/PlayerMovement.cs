c1806ffff7c0d4c6bf2e2ccaa3c835c8d59187f7
ofsonmez <sfehdev@gmail.com>
2021-04-18 00:00:16 +0300

    Game skeleton and characters animations added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb; // for velocity and forces our player
+
+    public Animator anim;
+
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    private float mx; // movement x-axis;
+
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+
+        if (Math.Abs(mx) > 0.05f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        
+        if (mx > 0.05f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        } 
+        else if (mx < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        
+        anim.SetBool("isGrounded", IsGrounded());
+    }
+    
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+    
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
