c2f56d009a49d02b35424a653b27bcbd0b2228c5
Blueshifty <blueshift.zoso@gmail.com>
2021-04-30 12:48:38 +0300

    Level Generation Added

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase tile;
+
+    public int width;
+    public int height;
+
+    private void Start()
+    {
+        ClearMap();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (height > width)
+            {
+                var swapHeight = height;
+                height = width;
+                width = swapHeight;
+            }
+
+            ClearMap();
+            GenerateMap(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (height < width)
+            {
+                var swapHeight = height;
+                height = width;
+                width = swapHeight;
+            }
+
+            ClearMap();
+            GenerateMap(true);
+        }
+    }
+
+    public void GenerateMap(bool sideWays)
+    {
+        ClearMap();
+        int[,] map = new int[height, width];
+        float seed;
+        seed = Time.time;
+        map = MapFunctions.GenerateArray(width, height, true);
+        map = sideWays ? MapFunctions.PerlinNoiseSideWays(map, seed) : MapFunctions.PerlinNoise(map, seed);
+        MapFunctions.RenderMap(map, tilemap, tile);
+    }
+
+    public void ClearMap()
+    {
+        tilemap.ClearAllTiles();
+        for (int i = 0; i <= 10; ++i)
+        {
+            tilemap.SetTile(new Vector3Int(i, height + 1, 0), tile);
+        }
+    }
+}
---
+++ Assets/Scripts/MapFunctions.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapFunctions
+{
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+
+        for (int i = 0; i < map.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < map.GetUpperBound(1); ++j)
+            {
+                if (empty)
+                {
+                    map[i, j] = 0;
+                }
+                else
+                {
+                    map[i, j] = 1;
+                }
+            }
+        }
+
+        return map;
+    }
+
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        tilemap.ClearAllTiles();
+
+        for (int i = 0; i < map.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < map.GetUpperBound(1); ++j)
+            {
+                if (map[i, j] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), tile);
+                }
+            }
+        }
+    }
+
+    public static void RenderMapWithOffset(int[,] map, Tilemap tilemap, TileBase tile, Vector2Int offset)
+    {
+        for (int i = 0; i < map.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < map.GetUpperBound(1); ++j)
+            {
+                if (map[i, j] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(i + offset.x, j + offset.y, 0), tile);
+                }
+            }
+        }
+    }
+
+    public static void UpdateMap(int[,] map, Tilemap tilemap)
+    {
+        for (int i = 0; i < map.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < map.GetUpperBound(1); ++j)
+            {
+                if (map[i, j] == 0)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), null);
+                }
+            }
+        }
+    }
+
+    public static int[,] PerlinNoise(int[,] map, float seed)
+    {
+        int newPoint;
+
+        float reduction = 0.5f;
+
+        for (int i = 0; i < map.GetUpperBound(0); ++i)
+        {
+            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(i, seed) - reduction) * map.GetUpperBound(1));
+            newPoint += (map.GetUpperBound(1) / 2);
+            for (int j = newPoint; j >= 0; --j)
+            {
+                map[i, j] = 1;
+            }
+        }
+
+        return map;
+    }
+
+    public static int[,] PerlinNoiseSideWays(int[,] map, float seed)
+    {
+        int newPoint;
+
+        float reduction = 0.5f;
+
+        for (int i = 0; i < map.GetUpperBound(1); ++i)
+        {
+            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(i, seed) - reduction) * (map.GetUpperBound(0) / 5 * 2));
+            newPoint += map.GetUpperBound(0) / 5 * 2;
+
+
+            for (int j = newPoint; j >= 0; --j)
+            {
+                map[j, i] = 1;
+            }
+
+            for (int j = newPoint + map.GetUpperBound(0) / 5; j < map.GetUpperBound(0); ++j)
+            {
+                map[j, i] = 1;
+            }
+        }
+
+        return map;
+    }
+}
